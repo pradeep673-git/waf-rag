1a3752717de3393ef0f1160c8fb9252d986b5001
PRADEEP <pradeep673-git>
2025-05-12 17:32:13 +0530

    opsm_project

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.middleware import WAFMiddleware
+
+app = FastAPI()
+app.add_middleware(WAFMiddleware)
+
+@app.get("/")
+
+def home():
+    return {"message": "this is a waf and it is working"}
